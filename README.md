968b6a33b232ec2fbd681e35794f90bdbcb52227
manneting <manneting@163.com>
2017-01-20 16:15:52 +0800

    增加了处理器

--- README.md
+++ README.md
@@ -15,3 +15,28 @@ ftp，sftp，file，smb（共享目录）
 新增加了http和https协议
 
 需要帮助可以联系QQ:646761506或者在 //138.xjee.cn/dc/ 上发留言给我！
+
+DeepWay依赖mongodb和redis，因此如果需要启动需要安装它们，相关的配置文件在deepway.properties文件中
+
+下面的test代码可以看出启动，暂停路由操作
+		
+	@Test
+	public void startRout() throws Exception {
+		deepRouteService.startRoute();//启动全部路由
+		logger.info("============================");
+		
+		List<DeepRoute>  routes = deepRouteService.findAll();
+		for (DeepRoute deepRoute : routes) {
+			String routeId = deepRoute.getRouteid();
+			ServiceStatus serviceStatus = camelContext.getRouteStatus( routeId );
+			logger.info(String.format("Route ID:%s|Status:%s",routeId, serviceStatus));
+			if (ServiceStatus.Suspended.equals(serviceStatus)) {// 暂停
+				camelContext.resumeRoute(routeId);
+			} else if (ServiceStatus.Started.equals(serviceStatus)) {// 启动
+				camelContext.suspendRoute(routeId);//暂停路由
+			}
+			logger.info(String.format("Route ID:%s|Status:%s",routeId,  camelContext.getRouteStatus( routeId )));
+		}
+		
+	}
+	
--- src/test/java/com/deep/test/DeepTest.java
+++ src/test/java/com/deep/test/DeepTest.java
@@ -3,12 +3,9 @@ package com.deep.test;
 import java.util.List;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Processor;
 import org.apache.camel.ServiceStatus;
-import org.apache.camel.component.http4.HttpComponent;
 import org.apache.camel.model.RouteDefinition;
-import org.apache.camel.util.jsse.KeyManagersParameters;
-import org.apache.camel.util.jsse.KeyStoreParameters;
-import org.apache.camel.util.jsse.SSLContextParameters;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
@@ -78,42 +75,60 @@ public class DeepTest{
 		camelContext.addRouteDefinition(routeDefinition);
 		camelContext.start();//
 		logger.info("============================");
-		
-		
-//		RouteDefinition routeDefinition = new RouteDefinition();
-//		routeDefinition.from(myRoute.getCfrom());
-//		routeDefinition.to(myRoute.getCto());
-//		camelContext.addRoutes(routeDefinition);
 	}
 	
-	//https route
+	//国际招标公司公告
 	@Test
-	public void routeHttpsTest() throws Exception {
+	public void shabiddingTest() throws Exception {
 		DeepRoute myRoute = new DeepRoute();
 		myRoute.setRouteid("TestRoute");//id不能为空
-//		myRoute.setCfrom("timer://report?fixedRate=true&delay=0&period=1000");
+////		myRoute.setFrom("ftp://lwh:123@172.20.32.26/test?stepwise=true&ignoreFileNotFoundOrPermissionError=true&delete=true&consumer.delay=10s");
+//		myRoute.setCfrom("file:D:/test1/?delete=true");
+//		myRoute.setCto("file:E:/temp/outbox/");
+//		myRoute.setBackpath("file:E:/temp/outbox1/");//做个也是需要的
+//		myRoute.setProcessRef("myProcess");
+////	myRoute.setRouteid("abcdddd");
+		myRoute.setCfrom("timer://report?fixedRate=true&delay=0&period=1000");
 //		myRoute.setCfrom("timer://report?fixedRate=true&delay=0");
 //		myRoute.setCfrom("timer://report?fixedRate=false");
-		myRoute.setCfrom("direct:start");
+//		myRoute.setCfrom("direct:start");
 //		Object sslContextParameters = appContext.getBean("sslContextParameters");
-//		myRoute.setCto("https://www.oschina.net/p/deepway");
-		myRoute.setCto("http://finance.sina.com.cn/realstock/company/sh600606/nc.shtml");
+		myRoute.setCto("http://www.shabidding.com/Chinese/ywxx_more.asp?id=%E9%A1%B9%E7%9B%AE%E5%85%AC%E5%91%8A");
 		logger.info("++++++++++++++++++++++++++");
 		RouteDefinition routeDefinition = new RouteDefinition();
 		routeDefinition.from(myRoute.getCfrom());
 //		routeDefinition.process(myRoute.getProcessRef());
-		routeDefinition.setHeader("user-agent").simple("Mozilla/5.0",String.class);//这是关键
+//		routeDefinition.setHeader("User-Agent:Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
 		routeDefinition.to(myRoute.getCto());
-		routeDefinition.to("file://E:/Temp/outbox1/?fileName=${date:now:yyyy}/oschina_${date:now:yyyyMMddhhmmss}.html");//自动修改文件名
+		routeDefinition.to("file://E:/Temp/outbox1/?fileName=${date:now:yyyy}/baidu.com.${date:now:yyyyMMddhhmmss}.html");//自动修改文件名
 		camelContext.addRouteDefinition(routeDefinition);
 		camelContext.start();//
 		logger.info("============================");
+	}
+	
+	
+	@Test
+	public void routeHttpsTest() throws Exception {
+		DeepRoute myRoute = new DeepRoute();
+		myRoute.setRouteid("TestRoute");//id不能为空
+//		myRoute.setCfrom("timer://report?fixedRate=false");
+		myRoute.setCfrom("timer://simpleTimer?delay=2s&repeatCount=1");// Delay and RepeatCount timer options
+//		myRoute.setCfrom("direct:start");
+//		myRoute.setCto("https://www.oschina.net/p/deepway");
+		myRoute.setCto("https4://www.oschina.net/p/deepway");
+		logger.info("++++++++++++++++++++++++++");
+		RouteDefinition routeDefinition = new RouteDefinition();
+		routeDefinition.from(myRoute.getCfrom());
+		Processor gjzbProcess = (Processor)appContext.getBean("gjzbProcess");
 		
+		routeDefinition.setHeader("user-agent").simple("Mozilla/5.0",String.class);//这是关键
+		routeDefinition.to(myRoute.getCto());
+		routeDefinition.process(gjzbProcess);
+		routeDefinition.to("file://E:/Temp/outbox1/?fileName=${date:now:yyyy}/oschina_${date:now:yyyyMMddhhmmss}.html");//自动修改文件名
 		
-//		RouteDefinition routeDefinition = new RouteDefinition();
-//		routeDefinition.from(myRoute.getCfrom());
-//		routeDefinition.to(myRoute.getCto());
-//		camelContext.addRoutes(routeDefinition);
+		camelContext.addRouteDefinition(routeDefinition);
+		camelContext.start();//
+		logger.info("============================");
 	}
 	
 	
